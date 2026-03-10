f293641e6e4510f6d883aaf6befebc34ca8759f0
Alan <alan037w7@gmail.com>
2026-03-10 16:53:13 -0600

    Version 1.0�

--- app/page.tsx
+++ app/page.tsx
@@ -7,5 +7,5 @@ export default function Home() {
     <main className="flex items-center justify-center min-h-screen bg-slate-950 px-4 sm:px-6 lg:px-8">
       <LoginForm />
     </main>
-  );
+  ); 
 }
